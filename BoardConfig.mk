f821e2fb428804b9e1ddc3bd47679b7fee36d7cb
Wu Liangqing <wlq@rock-chips.com>
2023-10-18 03:15:43 +0000

    remove GKI config, definition in device/common/Boardconfig.mk
    Change-Id: I74ae2f5acf5b5f4f275c5fbff095b7067b7b9d1e
    
    Former-commit-id: 49cbe036bf565b40f483cf4820112426cc204f34

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -64,21 +64,6 @@ BOARD_USES_GENERIC_INVENSENSE := false
 
 TARGET_BOARD_PLATFORM_PRODUCT ?= tablet
 
-ifeq ($(strip $(BOARD_BUILD_GKI)), true)
-    # AB image definition
-    BOARD_USES_AB_IMAGE := true
-    BOARD_ROCKCHIP_VIRTUAL_AB_ENABLE := true
-    BOARD_ROCKCHIP_VIRTUAL_AB_COMPRESSION_WITH_GKI_ENABLE := true
-else
-    BOARD_USES_AB_IMAGE := false
-    BOARD_ROCKCHIP_VIRTUAL_AB_ENABLE := false
-    BOARD_ROCKCHIP_VIRTUAL_AB_COMPRESSION_WITH_GKI_ENABLE := false
-endif
-
-ifeq ($(strip $(BOARD_USES_AB_IMAGE)), true)
-    include device/rockchip/common/BoardConfig_AB.mk
-endif
-
 ENABLE_CPUSETS := true
 
 # Enable Dex compile opt as default
